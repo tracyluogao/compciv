baaa8bd1fb562727cee783244670a74f1c979b00
Tracy Gao <tracygao@corn21.stanford.edu>
2015-03-18 11:26:40 -0700

    Final Project

--- homework/final-project/matcher2.sh
+++ homework/final-project/matcher2.sh
@@ -1,12 +1,12 @@
 csv1=$1
 txt1=$2
 
-touch data-hold/matched1.csv
-echo "ID,Posted at,Screen name,Text" >> data-hold/matched1.csv
+touch data-hold/matched2.csv
+echo "ID,Posted at,Screen name,Text" >> data-hold/matched2.csv
 while read line
 do
-	grep $line $csv1 >> data-hold/matched1.csv
+        grep $line $csv1 >> data-hold/matched2.csv
 done < $txt1
 
-csvfix order -fn 'Text' data-hold/matched1.csv | sed -e '1d' > data-hold/matched1.txt
-rm data-hold/matched1.csv
+csvfix order -fn 'Text' data-hold/matched2.csv | sed -e '1d' > data-hold/matched2.txt
+rm data-hold/matched2.csv
